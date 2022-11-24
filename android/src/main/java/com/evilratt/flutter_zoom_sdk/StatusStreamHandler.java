6f4be6bd6a503181d2dcc6331cc1fafb189218bc
niki.satoshi <niki.satoshi@toiware.co.jp>
2022-11-24 17:13:53 +0900

    不足していたインターフェースの関数を実装

--- android/src/main/java/com/evilratt/flutter_zoom_sdk/StatusStreamHandler.java
+++ android/src/main/java/com/evilratt/flutter_zoom_sdk/StatusStreamHandler.java
@@ -9,6 +9,7 @@ import us.zoom.sdk.MeetingError;
 import us.zoom.sdk.MeetingService;
 import us.zoom.sdk.MeetingServiceListener;
 import us.zoom.sdk.MeetingStatus;
+import us.zoom.sdk.MeetingParameter;
 
 /**
  * This class implements the handler for the Zoom meeting event in the flutter event channel
@@ -23,15 +24,23 @@ public class StatusStreamHandler implements EventChannel.StreamHandler {
 
     @Override
     public void onListen(Object arguments, final EventChannel.EventSink events) {
-        statusListener = (meetingStatus, errorCode, internalErrorCode) -> {
+        statusListener = new MeetingServiceListener() {
+            @Override
+            public void onMeetingStatusChanged(MeetingStatus meetingStatus, int errorCode, int internalErrorCode) {
 
-            if(meetingStatus == MeetingStatus.MEETING_STATUS_FAILED &&
-                    errorCode == MeetingError.MEETING_ERROR_CLIENT_INCOMPATIBLE) {
-                events.success(Arrays.asList("MEETING_STATUS_UNKNOWN", "Version of ZoomSDK is too low"));
-                return;
+                if(meetingStatus == MeetingStatus.MEETING_STATUS_FAILED &&
+                        errorCode == MeetingError.MEETING_ERROR_CLIENT_INCOMPATIBLE) {
+                    events.success(Arrays.asList("MEETING_STATUS_UNKNOWN", "Version of ZoomSDK is too low"));
+                    return;
+                }
+
+                events.success(getMeetingStatusMessage(meetingStatus));
             }
 
-            events.success(getMeetingStatusMessage(meetingStatus));
+            @Override
+            public void onMeetingParameterNotification(MeetingParameter meetingParameter) {
+                // TODO: implements
+            }
         };
 
         this.meetingService.addListener(statusListener);
